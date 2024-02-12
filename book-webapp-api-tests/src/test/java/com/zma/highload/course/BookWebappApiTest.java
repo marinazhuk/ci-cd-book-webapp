af12b95bda64d8e90fdfb5ac3180f23b98626892
Maryna Zhuk <zma@ciklum.com>
2024-02-12 22:46:53 +0200

    updated CI/CD workflow

--- book-webapp-api-tests/src/test/java/com/zma/highload/course/BookWebappApiTest.java
+++ book-webapp-api-tests/src/test/java/com/zma/highload/course/BookWebappApiTest.java
@@ -3,6 +3,7 @@ package com.zma.highload.course;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.apache.hc.client5.http.classic.methods.HttpDelete;
 import org.apache.hc.client5.http.classic.methods.HttpGet;
 import org.apache.hc.client5.http.classic.methods.HttpPost;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
@@ -10,19 +11,26 @@ import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import java.io.IOException;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BookWebappApiTest {
 
     private final String url = "http://ec2-16-170-211-2.eu-north-1.compute.amazonaws.com:8080/books";
 
     @Test
+    @Order(1)
     public void getBookNotFound() throws IOException {
         // Given
-        final HttpUriRequest request = new HttpGet(url + "/1000");
+        final HttpUriRequest request = new HttpGet(url + "/1");
         // When
 
         try (CloseableHttpClient client = HttpClients.createDefault();
@@ -34,6 +42,7 @@ public class BookWebappApiTest {
     }
 
     @Test
+    @Order(2)
     public void createBook() throws IOException {
 
         final String json =
@@ -51,12 +60,51 @@ public class BookWebappApiTest {
             // Then
             assertThat(response.getCode(), equalTo(HttpStatus.SC_OK));
         }
+    }
+
+    @Test
+    @Order(3)
+    public void getBookFound() throws IOException {
+        // Given
+        final HttpUriRequest request = new HttpGet(url + "/1");
+        // When
+
+        try (CloseableHttpClient client = HttpClients.createDefault();
+             CloseableHttpResponse response = client
+                     .execute(request)) {
+            // Then
+            assertThat(response.getCode(), equalTo(HttpStatus.SC_OK));
+        }
+    }
+
+    @Test
+    @Order(4)
+    public void getAllBooks() throws IOException, ParseException {
 
         // Given
-        final HttpUriRequest getRequest = new HttpGet(url + "/1");
+        final HttpUriRequest request = new HttpGet(url);
         // When
+
         try (CloseableHttpClient client = HttpClients.createDefault();
-             CloseableHttpResponse response = client.execute(getRequest)) {
+             CloseableHttpResponse response = client
+                     .execute(request)) {
+            // Then
+            assertThat(response.getCode(), equalTo(HttpStatus.SC_OK));
+            assertThat(EntityUtils.toString(response.getEntity(), "UTF-8"),
+                    equalTo("[{\"id\":1,\"author\":\"John\",\"title\":\"title\",\"yearPublished\":2000}]"));
+        }
+    }
+
+    @Test
+    @Order(5)
+    public void deleteBook() throws IOException {
+        // Given
+        final HttpUriRequest request = new HttpDelete(url + "/1");
+        // When
+
+        try (CloseableHttpClient client = HttpClients.createDefault();
+             CloseableHttpResponse response = client
+                     .execute(request)) {
             // Then
             assertThat(response.getCode(), equalTo(HttpStatus.SC_OK));
         }
--- book-webapp/src/main/java/com/zma/highload/course/controller/BookController.java
+++ book-webapp/src/main/java/com/zma/highload/course/controller/BookController.java
@@ -2,7 +2,10 @@ package com.zma.highload.course.controller;
 
 import com.zma.highload.course.model.Book;
 import com.zma.highload.course.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
+    private final static Logger logger = LoggerFactory.getLogger(BookController.class);
     private final BookRepository repository;
 
     public BookController(BookRepository repository) {
@@ -23,12 +29,26 @@ public class BookController {
     @PostMapping
     public void createBook(@RequestBody Book book) {
         repository.save(book);
+        logger.info("Book created {}", book);
     }
 
     @GetMapping("/{id}")
     public Book getBook(@PathVariable int id) {
+        logger.info("Find by id={} book request", id);
         return repository.findById(id)
                 .orElseThrow(
                         () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "book not found"));
+    }
+
+    @GetMapping
+    public List<Book> getAllBook() {
+        logger.info("Get all books request");
+        return (List<Book>) repository.findAll();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable int id) {
+        logger.info("Delete by id={} book request", id);
+        repository.deleteById(id);
     }
 }
